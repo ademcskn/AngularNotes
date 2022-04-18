bde4deef58bc5fb95d66c8d38611581a346e39f6
ademcskn <ademcskn@hotmail.com>
2022-04-18 17:21:37 +0300

    Güncellendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
-import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { BrandComponent } from './components/brand/brand.component';
 import { CarComponent } from './components/car/car.component';
 
 @NgModule({
+  //bir component kullanacağımız zaman buraya ekliyoruz.
   declarations: [
     AppComponent,
-    ProductComponent,
-    CategoryComponent,
     NaviComponent,
     BrandComponent,
     CarComponent
   ],
+  //bizim yazmadığım modulleri de buraya yazıyoruz.
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
